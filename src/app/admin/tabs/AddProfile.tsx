d9cf7fcfa2c1be5b99c0c691cd87918892918e6b
priyesshrai <priyeshrai369@gmail.com>
2025-05-31 12:03:24 +0530

    feat: Add AddProfile and CandidateList components; enhance AdminTabs with new tab functionality

---
+++ src/app/admin/tabs/AddProfile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddProfile() {
+  return (
+    <div>AddProfile</div>
+  )
+}
--- src/app/admin/tabs/AdminTabs.tsx
+++ src/app/admin/tabs/AdminTabs.tsx
@@ -1,31 +1,69 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { DashboardProps } from '@/app/user/Dashboard/Dashboard'
+import Image from 'next/image';
+import CandidateList from './CandidateList';
+import AddProfile from './AddProfile';
 
 type TabConfig = {
-    key: string;
-    tabName: string;
-    // icon: React.ComponentType<{ isActive: boolean }>;
-    // component: React.ComponentType<DashboardProps>
+  key: string;
+  tabName: string;
+  icon: string;
+  component: React.ComponentType<DashboardProps>
 }
 
 export default function AdminTabs() {
   const tabConfig: TabConfig[] = [
     {
-      key: "overview",
-      tabName: "Overview",
-      // icon: "",
-      // component: ""
+      key: "candidate",
+      tabName: "Candidate List",
+      icon: "hgi hgi-stroke hgi-user-list",
+      component: CandidateList
     },
     {
-      key: "profile",
-      tabName: "Profile",
-      // icon: "",
-      // component: ""
+      key: "addProfile",
+      tabName: "Add New Profile",
+      icon: "hgi hgi-stroke hgi-user-add-01",
+      component: AddProfile
     }
-
   ]
+
+  const [selectedTab, setSelectedTab] = useState(0)
+  const ActiveTab = tabConfig[selectedTab].component;
+
+
   return (
-    <div>AdminTabs</div>
+    <section className='admin-dashboard-container'>
+      <div className="admin-dashboard-wraper">
+        <div className="admin-tabs-container">
+          <div className="admin-tab-top">
+            <Image src="/images/user/logo.png" width={500} height={100} alt='DreamPath Development' />
+          </div>
+          <div className="admin-tab-body">
+            {
+              tabConfig.map((tab, index) => (
+                <div className={`admin-tab-item ${selectedTab === index ? 'active' : ''}`}
+                  key={tab.key}
+                  onClick={() => setSelectedTab(index)}
+                >
+                  <i className={tab.icon}></i>
+                  <span>{tab.tabName}</span>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+
+        <div className="admin-component-container">
+          <div className="admin-component-wraper">
+            
+
+
+
+            <ActiveTab />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
