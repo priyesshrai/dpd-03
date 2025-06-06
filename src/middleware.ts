6d6b4c4afa5a8899e2dbf6cace2120ceb779194f
priyesshrai <priyeshrai369@gmail.com>
2025-06-06 12:20:39 +0530

    feat: enhance middleware to redirect users based on login type from the home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,19 @@ export async function middleware(request: NextRequest) {
   const isPublicPath = pathname === "/login";
   const isAdminPath = pathname.startsWith("/admin");
   const isUserPath = pathname.startsWith("/user");
+  const homePage = pathname === '/'
+
+  if (homePage && userData) {
+    if (userData.login_type === "superadmin") {
+      return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+    } else if (userData.login_type === "candidate") {
+      return NextResponse.redirect(new URL("/user/profile", request.url));
+    }
+  }
+
+  if (homePage && !userData) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
   if (isPublicPath && userData) {
     if (userData.login_type === "superadmin") {
@@ -42,5 +55,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/login", "/user/:path*"],
+  matcher: ["/", "/admin/:path*", "/login", "/user/:path*"],
 };
