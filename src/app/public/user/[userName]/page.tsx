7119434f5734c2e8a8c6f15a6ed3a47c43c37be2
priyesshrai <priyeshrai369@gmail.com>
2025-06-06 16:16:19 +0530

    feat: implement generateMetadata function to fetch and return user profile data

--- src/app/public/user/[userName]/page.tsx
+++ src/app/public/user/[userName]/page.tsx
@@ -1,11 +1,37 @@
+import axios from "axios";
 import UserProfile from "./UserProfile";
 
 
 export default async function page({ params }: { params: Promise<{ userName: string }> }) {
     const { userName } = await params;
-    
+
     return (
         <UserProfile userName={userName} />
     )
 }
 
+export async function generateMetadata({ params }: { params: Promise<{ userName: string }> }) {
+    const { userName } = await params;
+
+    try {
+        const response = await axios.get(`https://inforbit.in/demo/dpd/profile/${userName}`);
+        const userData = response.data.data;
+
+        const profileImageUrl = userData.profile_photo.startsWith('http')
+            ? userData.profile_photo
+            : `https://inforbit.in${userData.profile_photo}`;
+
+        return {
+            title: userData.name,
+            description: userData.profile_heading,
+            icons: {
+                icon: profileImageUrl
+            }
+        };
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        return {
+            title: 'User not found',
+        };
+    }
+}
