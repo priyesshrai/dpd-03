08a6c91c0ea90f96bfd26f6ab883278ee92f45a1
priyesshrai <priyeshrai369@gmail.com>
2025-06-14 16:58:06 +0530

    refactor: remove unused useState import in UpdateWorkExe component

--- src/app/admin/user/update/[username]/UpdateWorkExe.tsx
+++ src/app/admin/user/update/[username]/UpdateWorkExe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { UpdateFormData, UpdateWorkExperience } from '../../../../../../types';
 import LargeSpinner from '@/components/Spinner/LargeSpinner';
 import { motion, AnimatePresence } from 'framer-motion';
