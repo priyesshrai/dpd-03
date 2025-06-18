89ddb74bd95e10af00fc94f14e834113f743a6aa
priyesshrai <priyeshrai369@gmail.com>
2025-06-18 15:50:26 +0530

    feat: add ResetPassword component with form handling and validation

--- src/app/admin/user/update/[username]/page.tsx
+++ src/app/admin/user/update/[username]/page.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import UpdateTabs from './UpdateTabs';
+import axios from 'axios';
 
 export default async function UpdateUserInfoPage({ params }: { params: Promise<{ username: string }> }) {
   const { username } = await params;
   return (
-    <UpdateTabs userName={username}/>
+    <UpdateTabs userName={username} />
   )
+}
+
+export async function generateMetadata({ params }: { params: Promise<{ userName: string }> }) {
+  const { userName } = await params;
+
+  try {
+    const response = await axios.get(`https://inforbit.in/demo/dpd/profile/${userName}`);
+    const userData = response.data.data;
+
+    const profileImageUrl = userData.profile_photo.startsWith('http')
+      ? userData.profile_photo
+      : `https://inforbit.in${userData.profile_photo}`;
+
+    return {
+      title: userData.name,
+      description: userData.profile_heading,
+      icons: {
+        icon: profileImageUrl
+      }
+    };
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    return {
+      title: 'User not found',
+    };
+  }
 }
---
+++ src/app/update-password/[nid]/[token]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ResetPassword from './ResetPassword';
+
+interface PageParams {
+  params: {
+    nid: string;
+    token: string;
+  };
+}
+
+export default async function page({ params }: PageParams) {
+    const { nid, token } = params;
+    
+  return (
+    <ResetPassword nid={nid} token={token} />
+  )
+}
+
