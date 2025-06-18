0089f4ad07496736d45500a0f26847a8df05c012
priyesshrai <priyeshrai369@gmail.com>
2025-06-18 15:17:28 +0530

    feat: enhance reset password functionality with improved loading state and error handling

--- src/app/login/reset-password/page.tsx
+++ src/app/login/reset-password/page.tsx
@@ -1,15 +1,46 @@
 'use client'
 import Spinner from '@/components/Spinner/Spinner'
+import axios from 'axios'
 import Image from 'next/image'
 import React, { useState } from 'react'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function page() {
   const [userData, setUserData] = useState<string>("")
-  const [loading, setLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  async function handleSubmit() {
-    console.log(userData);
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    if (!userData) {
+      toast.error("Please Enter the Valid Email or Mobile No.")
+      return
+    }
+    const formData = new FormData()
+    formData.append("mobile_email", userData);
+
+    toast.promise(
+      axios.post("https://inforbit.in/demo/dpd/forgot-password", formData)
+        .then((response) => {
+          if (response?.data?.status) {
+            setUserData("");
+            setIsLoading(false);
+            return response.data.message;
+          }
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          const errorMessage = error.response?.data?.message || error.message;
+          throw errorMessage;
+        }),
+      {
+        loading: "Please Wait....",
+        success: (message) => message || "Reset Link sent Successfully",
+        error: (err) => err || "Unable to sent Reset Link."
+      }
+    );
+
   }
   return (
     <section className="login-container">
@@ -35,8 +66,8 @@ export default function page() {
 
           <div className="card-footer">
             <button type="button" onClick={handleSubmit}
-              disabled={!userData || loading}>
-              {loading ? <Spinner /> : "Verify"}
+              disabled={!userData || isLoading}>
+              {isLoading ? <Spinner /> : "Verify"}
             </button>
           </div>
         </div>
