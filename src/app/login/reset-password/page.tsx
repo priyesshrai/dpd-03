4bfffe72ab1b63e28216bd6f38970092410d2ce8
priyesshrai <priyeshrai369@gmail.com>
2025-06-19 14:37:35 +0530

    fix: update ResetPassword component to use form submission correctly

--- src/app/login/reset-password/page.tsx
+++ src/app/login/reset-password/page.tsx
@@ -49,7 +49,7 @@ function ResetPassword() {
   return (
     <section className="login-container">
       <div className="login-card">
-        <form className="login-wraper" onSubmit={handleSubmit}>
+        <div className="login-wraper">
           <div className="card-top">
             <Image src='/images/user/logo.png' width={300} height={27} alt="Dream Path Profile Builder" />
           </div>
@@ -68,11 +68,11 @@ function ResetPassword() {
           </div>
 
           <div className="card-footer">
-            <button type="button" disabled={!userData || isLoading}>
+            <button type="button" onClick={handleSubmit} disabled={!userData || isLoading}>
               {isLoading ? <Spinner /> : "Verify"}
             </button>
           </div>
-        </form>
+        </div>
       </div>
       <Toaster />
     </section>
