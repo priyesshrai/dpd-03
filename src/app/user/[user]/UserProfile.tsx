0855e4553555489c970ba5b311a5e4196a10beb6
priyesshrai <priyeshrai369@gmail.com>
2025-06-25 13:33:13 +0530

    feat: remove unused imports from UserProfile component to clean up code

--- src/app/user/[user]/UserProfile.tsx
+++ src/app/user/[user]/UserProfile.tsx
@@ -4,9 +4,7 @@ import Link from 'next/link'
 import React, { useEffect, useRef, useState } from 'react'
 import { motion } from "motion/react"
 import { usePathname } from 'next/navigation'
-import axios from 'axios'
 import toast, { Toaster } from 'react-hot-toast';
-import { useRouter } from 'next/navigation'
 import { ApiEducation, ApiProject, ApiSkill, ApiWorkExp, HeroProps } from '../../../../types'
 import { HeroSkeleton, ProjectSkeleton, SideBarSkeleton, SkillSkeleton} from '@/components/Skeleton/Skeleton'
 import Cookies from "js-cookie";
